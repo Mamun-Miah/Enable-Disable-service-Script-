56efa71f5fc41426601e46a48ead40e31675ce38
Mamun-Miah <111224231+Mamun-Miah@users.noreply.github.com>
2024-05-07 14:25:34 +0600

    Add files via upload

---
+++ Disable Service.sh
@@ -0,0 +1,87 @@
+pm disable com.tencent.ig/com.tencent.midas.oversea.newnetwork.service.APNetDetectService &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.analytics.AnalyticsJobService &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.analytics.AnalyticsService &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementJobService &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementService &>/dev/null
+pm disable com.tencent.ig/com.google.firebase.components.ComponentDiscoveryService &>/dev/null
+pm disable com.tencent.ig/com.google.firebase.iid.FirebaseInstanceIdService &>/dev/null
+pm disable com.tencent.ig/com.google.firebase.messaging.FirebaseMessagingService &>/dev/null
+pm disable com.tencent.ig/com.sirius.meemo.foreground_service.ForegroundService &>/dev/null
+pm disable com.tencent.ig/com.tencent.quantum.download.GCBGDownloadService &>/dev/null
+pm disable com.tencent.ig/com.tenecnt.quantum.IGFirebaseInstanceIDService &>/dev/null
+pm disable com.tencent.ig/com.sirius.flutter.im.MeemoBGService &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.auth.api.signin.RevocationBoundService &>/dev/null
+pm disable com.tencent.ig/com.tencent.ig.OBBDownloaderService &>/dev/null
+pm disable com.tencent.ig/com.amazonaws.mobileconnectors.s3.transferutility.TransferService &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.webview.qq.WebViewService &>/dev/null
+pm disable com.tencent.ig/com.shieldtunnel.svpn.XYVpnService &>/dev/null
+######ALL SERVICE BLOCKED######
+
+
+
+pm disable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.ChromeCustomTabs.ActionBroadcastReceiver &>/dev/null
+pm disable com.tencent.ig/com.tencent.ig.AlarmReceiver &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.analytics.AnalyticsReceiver &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementReceiver &>/dev/null
+pm disable com.tencent.ig/com.facebook.CurrentAccessTokenExpirationBroadcastReceiver &>/dev/null
+pm disable com.tencent.ig/com.google.firebase.iid.FirebaseInstanceIdReceiver &>/dev/null
+pm disable com.tencent.ig/com.epicgames.ue4.LocalNotificationReceiver &>/dev/null
+#####ALL BROADCAST BLOCKED######
+
+
+pm disable com.tencent.ig/com.facebook.FacebookContentProvider &>/dev/null
+pm disable com.tencent.ig/com.tencent.gcloud.ApolloProvider &>/dev/null
+pm disable com.tencent.ig/com.facebook.internal.FacebookInitProvider &>/dev/null
+pm disable com.tencent.ig/androidx.core.content.FileProvider &>/dev/null
+pm disable com.tencent.ig/com.google.firebase.provider.FirebaseInitProvider &>/dev/null
+pm disable com.tencent.ig/com.helpshift.support.providers.HelpshiftFileProvider &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.friend.IMSDKFileProvider &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.ads.MobileAdsInitProvider &>/dev/null
+pm disable com.tencent.ig/androidx.lifecycle.ProcessLifecycleOwnerInitializer &>/dev/null
+pm disable com.tencent.ig/com.tencent.quantum.share.QuantumFileProvider &>/dev/null
+pm disable com.tencent.ig/io.sentry.android.core.SentryPerformanceProvider &>/dev/null
+pm disable com.tencent.ig/dev.fluttercommunity.plus.share.ShareFileProvider &>/dev/null
+pm disable com.tencent.ig/com.vk.api.sdk.internal.VKInitProvider &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.common.wechat.WeChatFileProvider &>/dev/null
+
+###ALL CONTENT PROVIDER BLOCKED#######
+
+
+
+pm disable com.tencent.ig/com.tencent.connect.common.AssistActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.midas.oversea.business.APMallActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.midas.oversea.business.pay.APMidasProxyActivity &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.ads.AdActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.tauth.AuthActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.grobot.lite.ui.container.BridgeActivity &>/dev/null
+pm disable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.ChromeCustomTabs.ChromeCustomTabsActivity &>/dev/null
+pm disable com.tencent.ig/com.facebook.CustomTabActivity &>/dev/null
+pm disable com.tencent.ig/com.facebook.CustomTabMainActivity &>/dev/null
+pm disable com.tencent.ig/com.facebook.FacebookActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.grobot.lite.GRobotIsolateActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.grobot.lite.GRobotProcessActivity &>/dev/null
+pm disable com.tencent.ig/com.subao.androidapi.GameMasterActivity &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.common.api.GoogleApiActivity &>/dev/null
+pm disable com.tencent.ig/com.helpshift.support.HSReview &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.base.IMSDKProxyActivity &>/dev/null
+pm disable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.InAppBrowser.InAppBrowserActivity &>/dev/null
+pm disable com.tencent.ig/com.sirius.flutter.engine.LoadingActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.qcloud.logutils.LogActivity &>/dev/null
+pm disable com.tencent.ig/com.twitter.sdk.android.core.identity.OAuthActivity &>/dev/null
+pm disable com.tencent.ig/com.helpshift.support.activities.ParentActivity &>/dev/null
+pm disable com.tencent.ig/com.android.billingclient.api.ProxyBillingActivity &>/dev/null
+pm disable com.tencent.ig/com.sirius.flutter.im.SNSFlutterActivity &>/dev/null
+pm disable com.tencent.ig/com.google.android.gms.auth.api.signin.internal.SignInHubActivity &>/dev/null
+pm disable com.tencent.ig/com.yalantis.ucrop.UCropActivity &>/dev/null
+pm disable com.tencent.ig/com.vk.api.sdk.ui.VKCaptchaActivity &>/dev/null
+pm disable com.tencent.ig/com.vk.api.sdk.ui.VKConfirmationActivity &>/dev/null
+pm disable com.tencent.ig/com.vk.sdk.VKServiceActivity &>/dev/null
+pm disable com.tencent.ig/com.vk.api.sdk.ui.VKWebViewAuthActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.ig.wxapi.WXEntryActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.agent.wechat.WeChatAgentActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.login.webverify.WebVerifyActivity &>/dev/null
+pm disable com.tencent.ig/io.flutter.plugins.urllauncher.WebViewActivity &>/dev/null
+pm disable com.tencent.ig/com.tencent.imsdk.android.webview.qq.WebViewWithFavActivity &>/dev/null
+
+#####ALL ACTIVITY BLOCKED######
---
+++ Enable Service.sh
@@ -0,0 +1,87 @@
+pm enable com.tencent.ig/com.tencent.midas.oversea.newnetwork.service.APNetDetectService &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.analytics.AnalyticsJobService &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.analytics.AnalyticsService &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementJobService &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementService &>/dev/null
+pm enable com.tencent.ig/com.google.firebase.components.ComponentDiscoveryService &>/dev/null
+pm enable com.tencent.ig/com.google.firebase.iid.FirebaseInstanceIdService &>/dev/null
+pm enable com.tencent.ig/com.google.firebase.messaging.FirebaseMessagingService &>/dev/null
+pm enable com.tencent.ig/com.sirius.meemo.foreground_service.ForegroundService &>/dev/null
+pm enable com.tencent.ig/com.tencent.quantum.download.GCBGDownloadService &>/dev/null
+pm enable com.tencent.ig/com.tenecnt.quantum.IGFirebaseInstanceIDService &>/dev/null
+pm enable com.tencent.ig/com.sirius.flutter.im.MeemoBGService &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.auth.api.signin.RevocationBoundService &>/dev/null
+pm enable com.tencent.ig/com.tencent.ig.OBBDownloaderService &>/dev/null
+pm enable com.tencent.ig/com.amazonaws.mobileconnectors.s3.transferutility.TransferService &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.webview.qq.WebViewService &>/dev/null
+pm enable com.tencent.ig/com.shieldtunnel.svpn.XYVpnService &>/dev/null
+######ALL SERVICE BLOCKED######
+
+
+
+pm enable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.ChromeCustomTabs.ActionBroadcastReceiver &>/dev/null
+pm enable com.tencent.ig/com.tencent.ig.AlarmReceiver &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.analytics.AnalyticsReceiver &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementInstallReferrerReceiver &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.measurement.AppMeasurementReceiver &>/dev/null
+pm enable com.tencent.ig/com.facebook.CurrentAccessTokenExpirationBroadcastReceiver &>/dev/null
+pm enable com.tencent.ig/com.google.firebase.iid.FirebaseInstanceIdReceiver &>/dev/null
+pm enable com.tencent.ig/com.epicgames.ue4.LocalNotificationReceiver &>/dev/null
+#####ALL BROADCAST BLOCKED######
+
+
+pm enable com.tencent.ig/com.facebook.FacebookContentProvider &>/dev/null
+pm enable com.tencent.ig/com.tencent.gcloud.ApolloProvider &>/dev/null
+pm enable com.tencent.ig/com.facebook.internal.FacebookInitProvider &>/dev/null
+pm enable com.tencent.ig/androidx.core.content.FileProvider &>/dev/null
+pm enable com.tencent.ig/com.google.firebase.provider.FirebaseInitProvider &>/dev/null
+pm enable com.tencent.ig/com.helpshift.support.providers.HelpshiftFileProvider &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.friend.IMSDKFileProvider &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.ads.MobileAdsInitProvider &>/dev/null
+pm enable com.tencent.ig/androidx.lifecycle.ProcessLifecycleOwnerInitializer &>/dev/null
+pm enable com.tencent.ig/com.tencent.quantum.share.QuantumFileProvider &>/dev/null
+pm enable com.tencent.ig/io.sentry.android.core.SentryPerformanceProvider &>/dev/null
+pm enable com.tencent.ig/dev.fluttercommunity.plus.share.ShareFileProvider &>/dev/null
+pm enable com.tencent.ig/com.vk.api.sdk.internal.VKInitProvider &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.common.wechat.WeChatFileProvider &>/dev/null
+
+###ALL CONTENT PROVIDER BLOCKED#######
+
+
+
+pm enable com.tencent.ig/com.tencent.connect.common.AssistActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.midas.oversea.business.APMallActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.midas.oversea.business.pay.APMidasProxyActivity &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.ads.AdActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.tauth.AuthActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.grobot.lite.ui.container.BridgeActivity &>/dev/null
+pm enable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.ChromeCustomTabs.ChromeCustomTabsActivity &>/dev/null
+pm enable com.tencent.ig/com.facebook.CustomTabActivity &>/dev/null
+pm enable com.tencent.ig/com.facebook.CustomTabMainActivity &>/dev/null
+pm enable com.tencent.ig/com.facebook.FacebookActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.grobot.lite.GRobotIsolateActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.grobot.lite.GRobotProcessActivity &>/dev/null
+pm enable com.tencent.ig/com.subao.androidapi.GameMasterActivity &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.common.api.GoogleApiActivity &>/dev/null
+pm enable com.tencent.ig/com.helpshift.support.HSReview &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.base.IMSDKProxyActivity &>/dev/null
+pm enable com.tencent.ig/com.pichillilorenzo.flutter_inappwebview.InAppBrowser.InAppBrowserActivity &>/dev/null
+pm enable com.tencent.ig/com.sirius.flutter.engine.LoadingActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.qcloud.logutils.LogActivity &>/dev/null
+pm enable com.tencent.ig/com.twitter.sdk.android.core.identity.OAuthActivity &>/dev/null
+pm enable com.tencent.ig/com.helpshift.support.activities.ParentActivity &>/dev/null
+pm enable com.tencent.ig/com.android.billingclient.api.ProxyBillingActivity &>/dev/null
+pm enable com.tencent.ig/com.sirius.flutter.im.SNSFlutterActivity &>/dev/null
+pm enable com.tencent.ig/com.google.android.gms.auth.api.signin.internal.SignInHubActivity &>/dev/null
+pm enable com.tencent.ig/com.yalantis.ucrop.UCropActivity &>/dev/null
+pm enable com.tencent.ig/com.vk.api.sdk.ui.VKCaptchaActivity &>/dev/null
+pm enable com.tencent.ig/com.vk.api.sdk.ui.VKConfirmationActivity &>/dev/null
+pm enable com.tencent.ig/com.vk.sdk.VKServiceActivity &>/dev/null
+pm enable com.tencent.ig/com.vk.api.sdk.ui.VKWebViewAuthActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.ig.wxapi.WXEntryActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.agent.wechat.WeChatAgentActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.login.webverify.WebVerifyActivity &>/dev/null
+pm enable com.tencent.ig/io.flutter.plugins.urllauncher.WebViewActivity &>/dev/null
+pm enable com.tencent.ig/com.tencent.imsdk.android.webview.qq.WebViewWithFavActivity &>/dev/null
+
+#####ALL ACTIVITY BLOCKED######
